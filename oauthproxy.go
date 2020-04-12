9c5d11f8dda2e19ff8f65f6edabdb7d3d65d89ea
amiya-elear <amiyasantra@elear.solutions>
2020-04-12 13:02:02 +0530

    add log file

--- oauthproxy.go
+++ oauthproxy.go
@@ -520,8 +520,10 @@ func (p *OAuthProxy) GetRedirect(req *http.Request) (redirect string, err error)
 	redirect = req.Header.Get("X-Auth-Request-Redirect")
 	if req.Form.Get("rd") != "" {
 		redirect = req.Form.Get("rd")
+                logger.printf("############################## redirect = %q", redirect);
 	}
 	if !p.IsValidRedirect(redirect) {
+                logger.printf("############################## invalid redirect");
 		redirect = req.URL.Path
 		if strings.HasPrefix(redirect, p.ProxyPrefix) {
 			redirect = "/"
